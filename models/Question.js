e97d8386fcabe54ac1acc359d3cb8da04575e242
MOGBEYI David <mogbeyidavid@gmail.com>
2019-10-24 10:50:44 +0100

    Established the one question to many answers relationship

--- models/Question.js
+++ models/Question.js
@@ -4,26 +4,32 @@ const Schema = mongoose.Schema;
 
 
 const QuestionSchema = new Schema({
-	question: {
-		type: String,
-		required: true
-	},
-	views: {
-		type: Number,
-		default: 0
-	},
-	upVotes: {
-		type: Number,
-		default: 0
-	},
-	downVotes: {
-		type: Number,
-		default: 0
-	},
-	user: {
-		type: Schema.Types.ObjectId,
-		ref: "User"
-	}
+    question: {
+        type: String,
+        required: true
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    upVotes: {
+        type: Number,
+        default: 0
+    },
+    downVotes: {
+        type: Number,
+        default: 0
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    answers: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Answer"
+        }
+    ]
 }, {timestamps: true});
 
 // Creates the question model
