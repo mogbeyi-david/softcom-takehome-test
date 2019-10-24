fe929910ce2f05f1713d2e25e0345212aa182b7f
MOGBEYI David <mogbeyidavid@gmail.com>
2019-10-24 10:50:18 +0100

    Created the answer model

---
+++ models/Answer.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const AnswerSchema = new Schema({
+    answer: {
+        type: String,
+        required: true
+    },
+    upVotes: {
+        type: Number,
+        default: 0
+    },
+    downVotes: {
+        type: Number,
+        default: 0
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    question: {
+        type: Schema.Types.ObjectId,
+        ref: "Question"
+    }
+}, {timestamps: true});
+
+// Creates the answer model
+const Answer = mongoose.model("Answer", AnswerSchema);
+module.exports = Answer;
