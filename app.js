78151541865c6205bcab9e34df0ad2c59e6cd7db
MOGBEYI David <mogbeyidavid@gmail.com>
2019-10-24 10:58:23 +0100

    Initialized answer resource templete

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const errorHandler = require("./utility/error-handler");
 //Import routers
 const {userRouter: userRouterV1} = require("./api/v1/routes");
 const {questionRouter: questionRouterV1} = require("./api/v1/routes");
+const {answerRouter: answerRouterV1} = require("./api/v1/routes");
 
 // Connect to the database
 const Database = require("./config/database/Database");
@@ -29,24 +30,25 @@ app.use(cors());
 
 //Ping the API to ensure it is running.
 app.get("/health-check", (req, res) => {
-	return response.sendSuccess({res, message: "Health check passed successfully!!!"});
+    return response.sendSuccess({res, message: "Health check passed successfully!!!"});
 });
 
 //Bind app entry points to routers
 app.use("/api/v1/users", userRouterV1);
 app.use("/api/v1/questions", questionRouterV1);
+app.use("/api/v1/answers", answerRouterV1);
 
 // Use the error handling middleware as the last in the middleware stack
 app.use((error, req, res, next) => {
-	res.locals.message = error.message;
-	res.locals.error = process.env.NODE_ENV === "development" ? error : {};
-	winston.error(`${error.status || 500} - ${error.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
-	return errorHandler.handle(error, req, res, next);
+    res.locals.message = error.message;
+    res.locals.error = process.env.NODE_ENV === "development" ? error : {};
+    winston.error(`${error.status || 500} - ${error.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+    return errorHandler.handle(error, req, res, next);
 });
 
 // Declare port and run the server
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
-	console.log(`Server running on port: ${PORT}`);
+    console.log(`Server running on port: ${PORT}`);
 });
 module.exports = server;
