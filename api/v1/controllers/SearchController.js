f4f936fcdf1790b28fe3ada6aee052fee9ff159f
MOGBEYI David <mogbeyidavid@gmail.com>
2019-10-25 13:55:38 +0100

    Added search method for the question and answer resource

--- api/v1/controllers/SearchController.js
+++ api/v1/controllers/SearchController.js
@@ -2,7 +2,8 @@ const status = require("http-status");
 
 const response = require("../../../utility/response");
 const UserRepository = require("../../../repositories/UserRepository");
-
+const AnswerRepository = require("../../../repositories/AnswerRepository");
+const QuestionRepository = require("../../../repositories/QuestionRepository");
 
 class SearchController {
 
@@ -18,6 +19,50 @@ class SearchController {
         let hits;
         try {
             const result = await UserRepository.search(query);
+            if (!result || !result.hits) {
+                return response.sendError({res, message: `No results found for ${query}`});
+            }
+            hits = result.hits.hits;
+            return response.sendSuccess({res, message: `Search for ${query}`, body: hits});
+        } catch (e) {
+            next(e)
+        }
+    }
+
+    /**
+     *
+     * @param req
+     * @param res
+     * @param next
+     * @returns {Promise<*>}
+     */
+    async searchAnswer(req, res, next) {
+        const {query} = req.query;
+        let hits;
+        try {
+            const result = await AnswerRepository.search(query);
+            if (!result || !result.hits) {
+                return response.sendError({res, message: `No results found for ${query}`});
+            }
+            hits = result.hits.hits;
+            return response.sendSuccess({res, message: `Search for ${query}`, body: hits});
+        } catch (e) {
+            next(e)
+        }
+    }
+
+    /**
+     *
+     * @param req
+     * @param res
+     * @param next
+     * @returns {Promise<*>}
+     */
+    async searchQuestion(req, res, next) {
+        const {query} = req.query;
+        let hits;
+        try {
+            const result = await QuestionRepository.search(query);
             if (!result || !result.hits) {
                 return response.sendError({res, message: `No results found for ${query}`});
             }
