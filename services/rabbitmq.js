1b57d2bfc02f16ae42f5475bcad882c8a643b9a1
MOGBEYI David <mogbeyidavid@gmail.com>
2019-10-25 08:16:37 +0100

    Implemented the RabbitMq publisher

--- services/rabbitmq.js
+++ services/rabbitmq.js
@@ -1,6 +1,21 @@
+const connectionString = require("../config/rabbitmq/connection");
+const open = require('amqplib').connect(connectionString);
+
 class RabbitMqService {
-    static publish(queue, data){
-        
+
+    /**
+     *
+     * @param queue
+     * @param data
+     */
+    static publish(queue, data) {
+        open.then(function (conn) {
+            return conn.createChannel();
+        }).then(function (ch) {
+            return ch.assertQueue(queue).then(function (ok) {
+                return ch.sendToQueue(queue, data);
+            });
+        }).catch(console.warn);
     }
 }
 
