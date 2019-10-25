dc81e04e3e093ea4d05b14d7ed27d1758af2b360
MOGBEYI David <mogbeyidavid@gmail.com>
2019-10-25 12:40:00 +0100

    Moved reset password email to background service

--- api/v1/controllers/AuthController.js
+++ api/v1/controllers/AuthController.js
@@ -11,87 +11,86 @@ const UserRepository = require("../../../repositories/UserRepository");
 
 class AuthController {
 
-	/**
+    /**
      *
      * @param req
      * @param res
      * @param next
      * @returns {Promise<*>}
      */
-	async login(req, res, next) {
+    async login(req, res, next) {
 
-		const {error} = validateUser(req.body);
-		if (error) {
-			return response.sendError({res, message: error.details[0].message});
-		}
+        const {error} = validateUser(req.body);
+        if (error) {
+            return response.sendError({res, message: error.details[0].message});
+        }
 
-		let {email, password} = req.body;
-		try {
-			const user = await UserRepository.findByEmail(email);
-			if (!user) {
-				return response.sendError({res, message: "Email or Password is Incorrect"});
-			}
+        let {email, password} = req.body;
+        try {
+            const user = await UserRepository.findByEmail(email);
+            if (!user) {
+                return response.sendError({res, message: "Email or Password is Incorrect"});
+            }
 
-			const validPassword = await hasher.comparePasswords(password, user.password);
-			if (!validPassword) {
-				return response.sendError({res, message: "Email or Password is Incorrect"});
-			}
-			const token = user.generateJsonWebToken();
-			const result = _.pick(user, ["firstname", "lastname", "email"]);
-			res.header("x-auth-token", token);
-			return response.sendSuccess({res, body: result, message: "Login successful"});
-		} catch (e) {
-			next(e);
-		}
-	}
+            const validPassword = await hasher.comparePasswords(password, user.password);
+            if (!validPassword) {
+                return response.sendError({res, message: "Email or Password is Incorrect"});
+            }
+            const token = user.generateJsonWebToken();
+            const result = _.pick(user, ["firstname", "lastname", "email"]);
+            res.header("x-auth-token", token);
+            return response.sendSuccess({res, body: result, message: "Login successful"});
+        } catch (e) {
+            next(e);
+        }
+    }
 
-	/**
+    /**
      *
      * @param req
      * @param res
      * @param next
      * @returns {Promise<*>}
      */
-	async sendResetPasswordLink(req, res, next) {
-		try {
-			const {email} = req.body;
-			if (!email) return response.sendError({res, message: "Email is required"});
-			const user = await UserRepository.findByEmail(email);
-			if (user) {
-				await mailer.sendPasswordResetLink(user.email);
-			}
-			return response.sendSuccess({
-				res,
-				message: "Please check your email for further instructions on resetting your password"
-			});
-		} catch (e) {
-			next(e);
-		}
-	}
+    async sendResetPasswordLink(req, res, next) {
+        try {
+            const {email} = req.body;
+            if (!email) return response.sendError({res, message: "Email is required"});
+            const user = await UserRepository.findByEmail(email);
+            if (user) mailer.sendPasswordResetLink(user.email);
 
-	/**
+            return response.sendSuccess({
+                res,
+                message: "Please check your email for further instructions on resetting your password"
+            });
+        } catch (e) {
+            next(e);
+        }
+    }
+
+    /**
      *
      * @param req
      * @param res
      * @param next
      * @returns {Promise<*>}
      */
-	async resetPassword(req, res, next) {
-		try {
-			let {email} = req.query;
-			let {password, confirmPassword} = req.body;
-			if (password !== confirmPassword) return response.sendError({res, message: "Passwords do not match"});
-			let user = await UserRepository.findByEmail(email);
-			if (!user) return response.sendError({res, message: "User does not exist", statusCode: status.NOT_FOUND});
-			password = await hasher.encryptPassword(password);
-			user.password = password;
-			const result = await user.save();
-			user = _.pick(result, ["_id", "firstname", "lastname", "email"]);
-			return response.sendSuccess({res, message: "Password reset successfully", body: user});
-		} catch (e) {
-			next(e);
-		}
-	}
+    async resetPassword(req, res, next) {
+        try {
+            let {email} = req.query;
+            let {password, confirmPassword} = req.body;
+            if (password !== confirmPassword) return response.sendError({res, message: "Passwords do not match"});
+            let user = await UserRepository.findByEmail(email);
+            if (!user) return response.sendError({res, message: "User does not exist", statusCode: status.NOT_FOUND});
+            password = await hasher.encryptPassword(password);
+            user.password = password;
+            const result = await user.save();
+            user = _.pick(result, ["_id", "firstname", "lastname", "email"]);
+            return response.sendSuccess({res, message: "Password reset successfully", body: user});
+        } catch (e) {
+            next(e);
+        }
+    }
 
 }
 
--- utility/mailer.js
+++ utility/mailer.js
@@ -1,27 +1,17 @@
 require("dotenv").config();
-const nodemailer = require("nodemailer");
+const rabbitMqService = require("../services/rabbitmq");
+const queue = process.env.RESET_PASSWORD_QUEUE;
 
-let transport = nodemailer.createTransport({
-	host: process.env.MAILTRAP_HOST,
-	port: process.env.MAILTRAP_PORT,
-	auth: {
-		user: process.env.MAILTRAP_USERNAME,
-		pass: process.env.MAILTRAP_PASSWORD
-	}
-});
 
 class Mailer {
 
-	static async sendPasswordResetLink(email) {
-		const link = `${process.env.APP_URL}/api/v1/users/reset-password?${email}`;
-		const message = {
-			from: process.env.APP_EMAIL,
-			to: email,
-			subject: "Reset Your Password",
-			html: `Please click <a href=${link}>Here</a> to reset your password`
-		};
-		await transport.sendMail(message);
-	}
+    /**
+     *
+     * @param email
+     */
+    static sendPasswordResetLink(email) {
+        return rabbitMqService.publish(queue, {email});
+    }
 }
 
 module.exports = Mailer;
