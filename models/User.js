2e9c235cbff90554ef3f6d63adfa9de44242e5ee
MOGBEYI David <mogbeyidavid@gmail.com>
2019-10-25 12:20:19 +0100

    WIP: Implement search

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
+const mongoosastic = require("mongoosastic");
 const jwt = require("jsonwebtoken");
 const Schema = mongoose.Schema;
 
@@ -7,40 +8,53 @@ const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 
 const UserSchema = new Schema({
-	firstname: {
-		type: String,
-		required: true
-	},
-	lastname: {
-		type: String,
-		required: true
-	},
-	email: {
-		type: String,
-		required: true,
-		unique: true
-	},
-	password: {
-		type: String,
-		required: true,
-		minlength: 6
-	},
-	isAdmin: {
-		type: Boolean,
-		default: false
-	},
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
 }, {timestamps: true});
 
 UserSchema.methods.generateJsonWebToken = function () {
-	return jwt.sign({
-		userId: this._id,
-		firstname: this.firstname,
-		lastname: this.lastname,
-		email: this.email,
-		isAdmin: this.isAdmin
-	}, JWT_SECRET_KEY);
+    return jwt.sign({
+        userId: this._id,
+        firstname: this.firstname,
+        lastname: this.lastname,
+        email: this.email,
+        isAdmin: this.isAdmin
+    }, JWT_SECRET_KEY);
 };
+
+UserSchema.plugin(mongoosastic, {
+    "host": process.env.APP_URL,
+    "port": process.env.ELASTIC_SEARCH_PORT || 9200
+});
 
 // Creates the user model
 const User = mongoose.model("User", UserSchema);
+User.createMapping((err, mapping) => {
+    console.log('mapping created');
+});
+
+User.on('es-indexed', (err, result) => {
+    console.log('indexed to elastic search');
+});
+
 module.exports = User;
